de175d86fc2186f01287ae45565cf4f229c10bec
davidkronish <david.kronish@mail.mcgill.ca>
2020-12-24 23:45:17 -0500

    some changes

--- cracking_the_coding_interview/tester.java
+++ cracking_the_coding_interview/tester.java
@@ -1,8 +1,9 @@
 /**
  * Hello this is a tester for my algorithm practice. 
  * Lets commit this bitch. 
- * This is just a tester vibe for the kro branch
  */
+
+ //THIS IS MY KRO BRANCH TESTER
 
 
  
